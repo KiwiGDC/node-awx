55703cccde90b747897ea2299aab5a842742e65c
Yghore <yhgore@gmail.com>
2024-06-16 15:24:11 +0200

    doc(readme) feat(test file)

--- test/index.ts
+++ test/index.ts
@@ -1,33 +1,11 @@
-# node-awx
-
-AWX Api Interface for nodejs
-
-## Implementations list
-
-- Inventory
-  - Get
-  - List
-- Job Template
-  - Get
-  - List
-- Job
-  - Get
-  - List
-  - isFinished
-
-## Usage
-
-```ts
 import AnsibleApi from "../src/index.js";
 import {JobTemplate} from "../src/Api/Endpoint/Job/JobTemplate.js";
 import {Inventory} from "../src/Api/Endpoint/Inventory/Inventory.js";
 
-const AApi = new AnsibleApi("https://localhost:8043/api/v2/", {token: "TOKEN"});
+const AApi = new AnsibleApi("https://localhost:8043/api/v2/", {token: "8odXpgaXUyghyFNSdGOBjSiwPTjAm8"});
 
 const job = await (await JobTemplate.get(7)).launch()
 const inventory = await Inventory.get(1)
 
 console.log(inventory.inventory)
 console.log(await job.isFinished())
-
-```
