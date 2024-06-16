f97b615292fe9fc97890f4f4a3939150f272028c
Yghore <yhgore@gmail.com>
2024-06-16 23:41:29 +0200

    fix(export) fix(main package.json)

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
-import { AnsibleApi } from "./Api/Api.js";
-
-export default AnsibleApi
+export {JobTemplate} from "./Api/Endpoint/Job/JobTemplate.js";
+export {Job} from "./Api/Endpoint/Job/Job.js";
+export {Inventory} from "./Api/Endpoint/Inventory/Inventory.js";
+export { AnsibleApi } from "./Api/Api.js";
