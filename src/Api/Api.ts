f7b1cec70aa2bb124523ec5ec16e53d3f0451f46
Yghore <yhgore@gmail.com>
2024-06-24 22:09:27 +0200

    feat(url Class)

--- src/Api/Api.ts
+++ src/Api/Api.ts
@@ -2,7 +2,7 @@ import { AuthToken } from "../Auth/Auth.js";
 import {FetchApiError} from "./Error/FetchApiError.js";
 import {params} from "./utils.js";
 import {InstanceApiError} from "./Error/InstanceApiError.js";
-import { AllowedMethod } from "./ApiUrl.js";
+import {AllowedMethod, ApiUrl, ApiUrlType} from "./ApiUrl.js";
 
 export class AnsibleApi
 {
@@ -11,23 +11,14 @@ export class AnsibleApi
 
     private static INSTANCE: AnsibleApi|null;
 
-    static TEST : string = "ping/"
+    static TEST : ApiUrlType = {endpoint: "ping/"}
 
     private async testServer()
     {
         const errorMessage = "ANSIBLE SERVER NOT REACHABLE (verify url and status of server) (put url with protocol and 'api/v2/' at the end)"
-        try 
-        {
-        const {status} = (await this.fetchAPI(AnsibleApi.TEST))
-        if(status != 200)
-            throw new Error(errorMessage);
-        }
-        catch 
-        {
-            throw new Error(errorMessage);
-        }
-        
-        
+        const {status} = (await this.fetchAPI(ApiUrl.gURL(AnsibleApi.TEST)))
+        if(status != 200) throw new Error(errorMessage);
+
     }
 
     constructor(url: string, auth: AuthToken)
@@ -45,12 +36,13 @@ export class AnsibleApi
         return this.INSTANCE
     }
 
-    public async fetchAPI(endpoint: string, paramRequest: RequestInit|undefined|AllowedMethod = undefined) {
-        const urlEndPoint = new URL(endpoint, this.url)
-        if(!paramRequest || paramRequest == "GET") paramRequest = params(this.token)
-        else if(typeof paramRequest == "string") paramRequest = params(this.token, paramRequest)
-        
-        return await fetch(urlEndPoint, paramRequest);
+    public async fetchAD(url: ApiUrl): Promise<any>
+    {
+        return this.fetchData(await this.fetchAPI(url));
+    }
+
+    public async fetchAPI(url: ApiUrl) {
+        return await fetch(url.toString(), params(this.token, url.method));
     }
 
     public async fetchData(response: Response)
@@ -63,7 +55,7 @@ export class AnsibleApi
     {
 
         const statusCode = response.status.toString();
-        if(statusCode.startsWith('404')) throw new FetchApiError(`Resources not found : ${response.status}`)
+        if(statusCode.startsWith('404')) throw new FetchApiError(`Resources not found : ${statusCode}`)
         if(statusCode.startsWith('401')) throw new FetchApiError(`Unauthorized (token is set?) : ${(await response.json())?.detail}`)
         if(statusCode.startsWith('403')) throw new FetchApiError(`Forbidden, verify the token permission! ${(await response.json())?.detail}`)
         if(statusCode.startsWith('5')) throw new FetchApiError(`Unknown error : ${response.status}`)
