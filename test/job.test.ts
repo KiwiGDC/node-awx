64df9a25e2505c2603ca1e93bc91b2e95b36717c
Yghore <yhgore@gmail.com>
2024-06-24 23:13:33 +0200

    feat(test) feat(ts-alias) test:  - Add Jest, Babel (For Jest)  - Add Two tests ts-alias:  - Remove .js from import  - After compiling add .js into import/export to dist/**/*.js

--- test/job.test.ts
+++ test/job.test.ts
@@ -2,16 +2,17 @@ import {AnsibleApi} from "../src";
 
 beforeAll(() => {
 
-    new AnsibleApi("https://localhost:8043/api/v2/", {token: process.env.AWX_TOKEN ? process.env.AWX_TOKEN : "TOKEN"})
+    new AnsibleApi(process.env.AWX_URL ? process.env.AWX_URL : "https://localhost:8043/api/v2/", {token: process.env.AWX_TOKEN ? process.env.AWX_TOKEN : "TOKEN"})
 
 })
 
 test('GETINSTANCE - AnsibleApi', async () => {
-    expect(AnsibleApi.GETINSTANCE().url.toString()).toBe("https://localhost:8043/api/v2/")
+    expect(AnsibleApi.GETINSTANCE().url.toString()).toBeDefined()
 
 });
 
 test('TestServer - AnsibleApi', async () => {
     const status = await AnsibleApi.GETINSTANCE().testServer()
+    console.log(status)
     expect(status).toBe(200)
 })
